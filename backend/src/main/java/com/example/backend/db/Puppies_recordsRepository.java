0ee77ab68044e8a16004a80cf22b72ff121b09b3
Lizeth Buenrostro <lbuenrostro@basecampcodingacademy.org>
2018-04-16 09:06:52 -0500

    Modified Puppy_recordsRepository and removed the class where user could sell a puppy.

--- backend/src/main/java/com/example/backend/db/Puppies_recordsRepository.java
+++ backend/src/main/java/com/example/backend/db/Puppies_recordsRepository.java
@@ -44,26 +44,6 @@ public class Puppies_recordsRepository {
 
     }
 
-    public static puppy_records SellPuppy(Integer id, String puppy_img_url, String breed, String gender, String age, Integer price) {
-        try {
-            Connection con = Connect.getDB();
-            PreparedStatement statement = con.prepareStatement(
-                    "INSERT INTO puppy_records (id, puppy_img_url, breed, gender, age, price) VALUES (?, ?, ?, ?, ?) RETURNING *");
-            statement.setInt(1, id);
-            statement.setString(2, puppy_img_url);
-            statement.setString(3, breed);
-            statement.setString(4, gender);
-            statement.setString(5, age);
-            statement.setInt(6, price);
-            ResultSet result = statement.executeQuery();
-            result.next();
-            return new puppy_records(result.getInt("id"), result.getString("puppy_img_url"), result.getString("breed"),
-                    result.getString("gender"), result.getString("age"), result.getInt("price"));
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-            return null;
-        }
-    }
 
     public static boolean deletePuppy(Integer id) {
         try {
